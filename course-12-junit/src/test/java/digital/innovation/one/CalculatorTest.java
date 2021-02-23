ee510d56d641ec148752e150743f0a99c7497fd1
Fernando Vinícius Gianini Silva <gianini@angularlabs.com.br>
2021-02-23 00:04:31 -0300

    First playing - mockito

--- course-12-junit/src/test/java/digital/innovation/one/CalculatorTest.java
+++ course-12-junit/src/test/java/digital/innovation/one/CalculatorTest.java
@@ -3,6 +3,8 @@ package digital.innovation.one;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class CalculatorTest {
     @Test
@@ -10,5 +12,15 @@ class CalculatorTest {
         Calculator calculator = new Calculator();
         var sum = calculator.sum("1+3+5+7");
         assertEquals(16, sum);
+    }
+
+    @Test
+    void sumWithMock() {
+        Calculator calculator = mock(Calculator.class);
+
+        when(calculator.sum("1+2+3")).thenReturn(6);
+
+        int res = calculator.sum("1+2+3");
+        assertEquals(6, res);
     }
 }
